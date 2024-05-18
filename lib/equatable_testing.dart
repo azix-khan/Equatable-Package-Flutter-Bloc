a760c3b2523867dbbbbc44c6868978da8a547d84
azix khan <azixkhan.55@gmail.com>
2024-05-18 12:29:46 +0500

    using equatable package

--- lib/equatable_testing.dart
+++ lib/equatable_testing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:equatable/equatable.dart';
 
 class EquatableTesting extends StatefulWidget {
   const EquatableTesting({super.key});
@@ -12,33 +13,46 @@ class _EquatableTestingState extends State<EquatableTesting> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: () {
-        Person person = Person(name: 'azix', age: 19);
-        Person person1 = Person(name: 'azix', age: 19);
+        Person person = const Person(name: 'azix', age: 19);
+        Person person1 = const Person(name: 'azix', age: 19);
 
         print(person.hashCode.toString());
         print(person1.hashCode.toString());
 
         print(person == person1);
-        print(person == person);
+        // print(person == person);
       }),
     );
   }
 }
 
-class Person {
+//--->>> With Equatable Package
+class Person extends Equatable {
   final String name;
   final int age;
 
   const Person({required this.name, required this.age});
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Person &&
-          runtimeType == other.runtimeType &&
-          name == other.name &&
-          age == other.age;
-
-  @override
-  int get hashCode => name.hashCode ^ age.hashCode;
+  List<Object?> get props => [name, age];
 }
+
+//--->>> Without Equatable Package
+
+// class Person {
+//   final String name;
+//   final int age;
+
+//   const Person({required this.name, required this.age});
+
+//   @override
+//   bool operator ==(Object other) =>
+//       identical(this, other) ||
+//       other is Person &&
+//           runtimeType == other.runtimeType &&
+//           name == other.name &&
+//           age == other.age;
+
+//   @override
+//   int get hashCode => name.hashCode ^ age.hashCode;
+// }
